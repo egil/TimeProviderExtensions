fc8f43f5a923291d8f7839c45b1fa55b144a751d
Egil Hansen <egil@assimilated.dk>
2023-02-20 14:42:58 +0000

    test: verify wrapped PeriodicTimer throws OperationCancelledException when cancellation token is set

--- test/TimeScheduler.Tests/DefaultSchedulerTests.cs
+++ test/TimeScheduler.Tests/DefaultSchedulerTests.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Threading;
 
 namespace TimeScheduler;
 
@@ -21,7 +22,7 @@ public class DefaultSchedulerTests
         var timer = Stopwatch.StartNew();
 
         await sut.Delay(TimeSpan.FromSeconds(1));
-        
+
         timer.Stop();
         timer.Elapsed.Should().BeCloseTo(
             nearbyTime: TimeSpan.FromSeconds(1),
@@ -34,8 +35,29 @@ public class DefaultSchedulerTests
         var sut = new DefaultScheduler();
         using var periodTimer = sut.PeriodicTimer(TimeSpan.FromMilliseconds(5));
 
-        var result =await periodTimer.WaitForNextTickAsync(CancellationToken.None);
+        var result = await periodTimer.WaitForNextTickAsync(CancellationToken.None);
 
         result.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task PeriodicTimer_cancelled_throws()
+    {
+        var cts = new CancellationTokenSource();
+        var sut = new DefaultScheduler();
+        var timerTask = TimerTask(cts.Token);
+
+        cts.Cancel();
+        cts.Dispose();
+
+        await timerTask.Awaiting(x => x)
+            .Should()
+            .ThrowAsync<OperationCanceledException>();
+
+        async Task TimerTask(CancellationToken cancellationToken)
+        {
+            using var periodTimer = sut.PeriodicTimer(TimeSpan.FromSeconds(1));
+            await periodTimer.WaitForNextTickAsync(cancellationToken);
+        }
     }
 }
