ddd11e98658b0b0956f524ccb3aa304114143629
Egil Hansen <egil@assimilated.dk>
2023-05-19 12:43:21 +0000

    fix: remove CancelAfter extension methods

--- test/TimeProviderExtensions.Tests/ManualTimeProviderCancelAfter.cs.cs
+++ test/TimeProviderExtensions.Tests/ManualTimeProviderCancelAfter.cs.cs
@@ -2,107 +2,51 @@ namespace TimeProviderExtensions;
 
 public class ManualTimeProviderCancelAfter
 {
-    internal const uint MaxSupportedTimeout = 0xfffffffe;
-
-    [Theory]
-    [InlineData(-2)]
-    [InlineData(MaxSupportedTimeout + 1)]
-    public void CancelAfter_throws_ArgumentOutOfRangeException(double timespanInMilliseconds)
-    {
-        using var cts = new CancellationTokenSource();
-        var sut = new ManualTimeProvider();
-
-        var throws = () => cts.CancelAfter(TimeSpan.FromMilliseconds(timespanInMilliseconds), sut);
-
-        throws.Should().ThrowExactly<ArgumentOutOfRangeException>();
-    }
-
-    [Fact]
-    public void CancelAfter_throws_ArgumentNullException()
-    {
-        using var cts = new CancellationTokenSource();
-
-        var throws = () => cts.CancelAfter(TimeSpan.Zero, default(ManualTimeProvider)!);
-
-        throws.Should().ThrowExactly<ArgumentNullException>();
-    }
-
-    [Fact]
-    public void CancelAfter_does_nothing_when_cts_already_canceled()
-    {
-        using var cts = new CancellationTokenSource();
-        var sut = new ManualTimeProvider();
-
-        cts.Cancel();
-        cts.CancelAfter(TimeSpan.Zero, sut);
-
-        cts.IsCancellationRequested.Should().BeTrue();
-    }
-
-    [Fact]
-    public void CancelAfter_does_nothing_when_delay_eq_zero()
-    {
-        using var cts = new CancellationTokenSource();
-        var sut = new ManualTimeProvider();
-
-        cts.CancelAfter(TimeSpan.Zero, sut);
-
-        cts.IsCancellationRequested.Should().BeFalse();
-    }
-
-    [Fact]
-    public void CancelAfter_does_nothing_when_delay_infinite()
-    {
-        using var cts = new CancellationTokenSource();
-        var sut = new ManualTimeProvider();
-
-        cts.CancelAfter(TimeSpan.FromMilliseconds(-1), sut);
-
-        cts.IsCancellationRequested.Should().BeFalse();
-    }
-
     [Fact]
     public void CancelAfter_cancels()
     {
-        using var cts = new CancellationTokenSource();
-        var sut = new ManualTimeProvider();
         var delay = TimeSpan.FromMilliseconds(42);
-
-        cts.CancelAfter(delay, sut);
+        var sut = new ManualTimeProvider();
+        using var cts = sut.CreateCancellationTokenSource(delay);
 
         sut.ForwardTime(delay);
+
         cts.IsCancellationRequested.Should().BeTrue();
     }
 
+#if NET8_0_OR_GREATER
+    // The following two scenarios are only supported by .NET 8 and up.
     [Fact]
     public void CancelAfter_reschedule_longer_cancel()
     {
-        using var cts = new CancellationTokenSource();
-        var sut = new ManualTimeProvider();
         var initialDelay = TimeSpan.FromMilliseconds(100);
         var rescheduledDelay = TimeSpan.FromMilliseconds(1000);
+        var sut = new ManualTimeProvider();
+        using var cts = sut.CreateCancellationTokenSource(initialDelay);
 
-        cts.CancelAfter(initialDelay, sut);
-        cts.CancelAfter(rescheduledDelay, sut);
+        cts.CancelAfter(rescheduledDelay);
 
         sut.ForwardTime(initialDelay);
         cts.IsCancellationRequested.Should().BeFalse();
-        sut.ForwardTime(rescheduledDelay);
+
+        sut.ForwardTime(rescheduledDelay - initialDelay);
         cts.IsCancellationRequested.Should().BeTrue();
     }
 
     [Fact]
-    public void CancelAfter_reschedule_shorter_cancel()
+    public async void CancelAfter_reschedule_shorter_cancel()
     {
-        using var cts = new CancellationTokenSource();
-        var sut = new ManualTimeProvider();
         var initialDelay = TimeSpan.FromMilliseconds(1000);
         var rescheduledDelay = TimeSpan.FromMilliseconds(100);
+        var sut = new ManualTimeProvider();
+        using var cts = sut.CreateCancellationTokenSource(initialDelay);
 
-        cts.CancelAfter(initialDelay, sut);
-        cts.CancelAfter(rescheduledDelay, sut);
+        cts.CancelAfter(rescheduledDelay);
 
         sut.ForwardTime(rescheduledDelay);
+        await Task.Delay(1000);
+
         cts.IsCancellationRequested.Should().BeTrue();
     }
+#endif
 }
--- test/TimeProviderExtensions.Tests/ManualTimeProviderTests.cs
+++ test/TimeProviderExtensions.Tests/ManualTimeProviderTests.cs
@@ -25,18 +25,19 @@ public class ManualTimeProviderTests
     }
 
     [Fact]
-    public void Delay_callbacks_runs_synchronously()
+    public async Task Delay_callbacks_runs_synchronously()
     {
         // arrange
         var sut = new ManualTimeProvider();
         var callbackCount = 0;
-        _ = Continuation(sut, () => callbackCount++);
+        var continuationTask = Continuation(sut, () => callbackCount++);
 
         // act
         sut.ForwardTime(TimeSpan.FromSeconds(10));
 
         // assert
         callbackCount.Should().Be(1);
+        await continuationTask;
 
         static async Task Continuation(TimeProvider timeProvider, Action callback)
         {
@@ -46,24 +47,26 @@ public class ManualTimeProviderTests
     }
 
     [Fact]
-    public void WaitAsync_callbacks_runs_synchronously()
+    public async Task WaitAsync_callbacks_runs_synchronously()
     {
         // arrange
         var sut = new ManualTimeProvider();
         var callbackCount = 0;
-        _ = Continuation(sut, () => callbackCount++);
+        var continuationTask = Continuation(sut, () => callbackCount++);
 
         // act
         sut.ForwardTime(TimeSpan.FromSeconds(10));
 
         // assert
         callbackCount.Should().Be(1);
+        await continuationTask;
 
         static async Task Continuation(TimeProvider timeProvider, Action callback)
         {
             try
             {
-                await Task.Delay(TimeSpan.FromDays(1)).WaitAsync(TimeSpan.FromSeconds(10), timeProvider);
+                await Task.Delay(TimeSpan.FromDays(1))
+                    .WaitAsync(TimeSpan.FromSeconds(10), timeProvider);
             }
             catch (TimeoutException)
             {
